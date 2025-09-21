6f8160621b59635b91a794a8b3643e030ee9bf3d
Salvador6699 <ganbaru6699@hotmail.com>
2025-09-21 09:21:52 +0200

    mejoramos el menú de navegación responsivo, actualizando el botón de desconexión y simplificando el código

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -77,34 +77,29 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600"  style="background-color:red">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
+        <div class="border-t border-gray-200 dark:border-gray-600">
             <div class="mt-3 space-y-1">
+                <!-- Authentication -->
+                <button class="mx-2 my-1 p-2xl flex justify-center items-center bg-accent"
+                    style="border: 2px solid var(--accent); border-radius: 8px;">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                            <span class="text-xl font-medium text-primary dark:text-gray-900"><i
+                                    class="fa-solid fa-right-from-bracket mr-4"></i>{{ __('Disconnect') }}</span>
+                        </x-responsive-nav-link>
+                    </form>
+                </button>
+
+                <!-- Links -->
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                        onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
     </div>
